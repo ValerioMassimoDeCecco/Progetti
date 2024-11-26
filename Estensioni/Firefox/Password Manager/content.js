9dfa11338708f01adb1733631259c23218765efa
Valerio <dececcomassimovalerio@gmail.com>
2024-11-26 09:17:50 +0100

    estensione ripristinata

--- Estensioni/Firefox/Password Manager/content.js
+++
@@ -1,9 +0,0 @@
-// Quando una nuova pagina viene caricata, invia l'URL al background
-chrome.runtime.sendMessage({
-  type: 'LOG_SITE',
-  data: {
-    url: window.location.href,
-    title: document.title,
-    timestamp: new Date().toISOString()
-  }
-});
